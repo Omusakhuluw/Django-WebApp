40bbf676a1a9f7410fed27b0a987dbff17603560
omusakhulu <omusakhuluw@gmail.com>
2024-02-25 01:10:51 +0300

    further updates on link pages urls

--- SokoFarm/SokoPr/SokoApp/views.py
+++ SokoFarm/SokoPr/SokoApp/views.py
@@ -23,4 +23,22 @@ def checkout(request):
 def contact(request):
     return render(request, 'contact.html')
 
+from django.shortcuts import render
+
+def my_view(request):
+    Facebook = "https://www.facebook.com/sharer/sharer.php?u"  # Replace with your actual URL
+    return render(request, 'home.html', 'about.html', 'detail.html', 'shop.html', 'cart.html', 'checkout.html', 'contact.html', {'https://www.facebook.com/sharer/sharer.php?u': Facebook})
+
+def my_view(request):
+    Twitter = "https://twitter.com/intent/tweet?url"  # Replace with your actual URL
+    return render(request, 'home.html', 'about.html', 'detail.html', 'shop.html', 'cart.html', 'checkout.html', 'contact.html', {'https://twitter.com/intent/tweet?url': Twitter})
+
+def my_view(request):
+    LinkedIn = "https://www.linkedin.com/sharing/share-offsite/?url"  # Replace with your actual URL
+    return render(request, 'home.html', 'about.html', 'detail.html', 'shop.html', 'cart.html', 'checkout.html', 'contact.html', {'https://www.linkedin.com/sharing/share-offsite/?url': LinkedIn})
+
+def my_view(request):
+    Instagram = "https://www.instagram.com/your_username"  # Replace with your actual URL
+    return render(request, 'home.html', 'about.html', 'detail.html', 'shop.html', 'cart.html', 'checkout.html', 'contact.html', {'https://www.instagram.com/your_username': Instagram})
+
 # Create your views here.
