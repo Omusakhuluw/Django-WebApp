f36b150224ca7e0fd4234073ae2ee60207fa177a
omusakhulu <omusakhuluw@gmail.com>
2024-02-25 02:41:28 +0300

    tired

--- SokoFarm/SokoPr/SokoPr/settings.py
+++ SokoFarm/SokoPr/SokoPr/settings.py
@@ -129,6 +129,13 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'omusakhuluw@gmail.com'
+EMAIL_HOST_PASSWORD = 'projectx'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
