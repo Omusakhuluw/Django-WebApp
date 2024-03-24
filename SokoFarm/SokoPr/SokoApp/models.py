4386db4cc280ff1ab0e9d89e6745eab022dd4501
omusakhulu <omusakhuluw@gmail.com>
2024-03-25 01:15:19 +0300

    changes on checkout and login forms

--- SokoFarm/SokoPr/SokoApp/models.py
+++ SokoFarm/SokoPr/SokoApp/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    is_farmer = models.BooleanField(default=False)
+    is_buyer = models.BooleanField(default=False)
+    is_guest = models.BooleanField(default=False)
 
 
 class Category(models.Model):
